f84b5aefd56b639b24742239cc8a864be56ab825
R Zhao <arcus.zhao@gmail.com>
2014-05-19 11:36:25 -0700

    Move into context menu

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({ file: 'sort.js' })
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  (info.menuItemId == "markdown_sort") && chrome.tabs.executeScript({ file: 'sort.js' });
 });
 
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({"title": "Sort", "contexts":["editable"], "id": "markdown_sort"});
+});
